7c64895b78eb9db6511c8bd900b9838da6e234d9
FullIdle <2675423979@qq.com>
2026-03-06 01:02:21 +0800

    HUD支持键盘监听

---
+++ client/common/src/main/java/org/figsq/taichicore/taichicore/mixin/MixinKeyboardHandler.java
@@ -0,0 +1,41 @@
+package org.figsq.taichicore.taichicore.mixin;
+
+import lombok.val;
+import net.minecraft.client.KeyboardHandler;
+import org.figsq.taichicore.taichicore.TaiChiCore;
+import org.lwjgl.glfw.GLFW;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Set;
+
+@Mixin(KeyboardHandler.class)
+public class MixinKeyboardHandler {
+    @Unique
+    private static final Set<Integer> LIMIT_KEYS = Set.of(
+            GLFW.GLFW_KEY_ESCAPE
+    );
+
+    @Inject(method = "keyPress", at = @At("HEAD"))
+    private void keyPress(long window, int key, int scanCode, int action, int modifiers, CallbackInfo ci) {
+        val minecraft = TaiChiCore.HUD.getMinecraft();
+        if (!TaiChiCore.renderHUD || minecraft == null || minecraft.screen != null) return;
+
+        if (LIMIT_KEYS.contains(key)) return;
+
+        if (action == GLFW.GLFW_PRESS || action == GLFW.GLFW_REPEAT)
+            TaiChiCore.HUD.keyPressed(key, scanCode, modifiers);
+        else if (action == GLFW.GLFW_RELEASE)
+            TaiChiCore.HUD.keyReleased(key, scanCode, modifiers);
+    }
+
+    @Inject(method = "charTyped", at = @At("HEAD"))
+    private void onCharTyped(long window, int codePoint, int modifiers, CallbackInfo ci) {
+        val minecraft = TaiChiCore.HUD.getMinecraft();
+        if (!TaiChiCore.renderHUD || minecraft == null || minecraft.screen != null) return;
+        TaiChiCore.HUD.charTyped(((char) codePoint), modifiers);
+    }
+}
